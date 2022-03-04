81fb334eeb4e05794453abf6afd5a98353e661a9
saepp <dsrisk911@gmail.com>
2022-03-04 18:44:38 +0700

    add swr

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Search from "../components/Search";
 
-export default function Home({ provinces }) {
+export default function Home() {
   return (
     <div className="bg-white dark:bg-[#362D3D]">
       <Search />
