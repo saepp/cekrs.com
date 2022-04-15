afb34acf059d28a68900ad1f8a07e86e9332ce4a
saepp <dsrisk911@gmail.com>
2022-04-15 13:53:08 +0700

    install nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import Router from "next/router";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
